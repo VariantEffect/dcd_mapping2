5b3c2fb8da17767b0b2138289405ccd30e761931
Sally Grindstaff <sallybg@uw.edu>
2024-08-26 14:40:00 -0700

    Add API docstrings

--- src/api/server_main.py
+++ src/api/server_main.py
@@ -11,5 +11,4 @@ app.include_router(map.router)
 
 # If the application is not already being run within a uvicorn server, start uvicorn here.
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # noqa: S104
