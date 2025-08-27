36273a8b6dc8957afa438c9f54c1d9e606fe7f9b
Ben Capodanno <capodb@uw.edu>
2025-08-27 13:06:14 -0700

    feat: configure server logging on startup

--- src/api/server_main.py
+++ src/api/server_main.py
@@ -1,12 +1,25 @@
 """FastAPI server file"""
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 
 from api.routers import map
+from dcd_mapping import dcd_mapping_version
+
+logging.basicConfig(
+    format="%(levelname)s:%(name)s:%(message)s",
+    level=logging.INFO,
+    force=True,
+)
+_logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 app.include_router(map.router)
+
+msg = f"Starting DCD Mapping server v{dcd_mapping_version})"
+_logger.info(msg)
 
 
 # If the application is not already being run within a uvicorn server, start uvicorn here.
